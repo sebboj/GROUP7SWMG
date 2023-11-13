6f19c10c0209801929e8cf553f1cb770933ef58d
DeFraCri <143744149+DeFraCri@users.noreply.github.com>
2023-11-12 20:38:57 -0500

    Delete SearchApp directory

--- SearchApp/SearchApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    isbn = models.CharField(max_length=13, unique=True)
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-
-    class Meta:
-        app_label = 'SearchApp'
